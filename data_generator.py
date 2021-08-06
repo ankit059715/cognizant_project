e489a32ffbf3805912bb144b325d6143f3a6f4cc
ankit singh <ankit059715@gmail.com>
2021-08-06 21:46:15 +0530

    Project Setup

---
+++ data_generator.py
@@ -0,0 +1,17 @@
+import string
+import random
+
+
+class DataGenerator:
+
+    def __init__(self, number_data=1000):
+        self.number_data = 1000
+
+    def generate_data():
+        pass
+
+
+if __name__ == "__main__":
+    data_generator = DataGenerator()
+    data = data_generator.generate_data()
+    
